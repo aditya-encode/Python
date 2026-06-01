bcd5d122c4c484eb34da441221a8427024e933e9
Aditya <adipatel2901@gmail.com>
2026-06-01 23:58:08 +0530

    uncommented the reverse.py in dsa

--- DSA/LIsts/reverse.py
+++ DSA/LIsts/reverse.py
@@ -1,14 +1,14 @@
-# class Solution:
+class Solution:
 
-#     def reverseArray(self, arr):
-#         left=0
-#         right=len(arr) - 1
-#         while left<right:
-#             arr[left],arr[right]=arr[right],arr[left]
-#             left+=1
-#             right-=1
-# a=Solution()
-# abc=[1,2,3,4,5]
-# a.reverseArray(abc)
-# print(abc)
+    def reverseArray(self, arr):
+        left=0
+        right=len(arr) - 1
+        while left<right:
+            arr[left],arr[right]=arr[right],arr[left]
+            left+=1
+            right-=1
+a=Solution()
+abc=[1,2,3,4,5]
+a.reverseArray(abc)
+print(abc)
 
